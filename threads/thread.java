58ff5fb51072b602d9454d4d1e16612da129c552
vikascselab <cse9565@gmail.com>
2025-09-23 16:30:07 +0530

    Re-added threads as a normal folder instead of submodule

---
+++ threads/thread.java
@@ -0,0 +1,29 @@
+
+public class thread {
+  public static void main(String[] args) {
+
+    System.out.println("weclome to treads");
+    long startime = System.currentTimeMillis();
+    // first task
+
+    for (int i = 0; i <= 1000; i++) {
+
+      System.out.print("*" + i + " ");
+    }
+
+    for (int i = 0; i <= 1000; i++) {
+
+      System.out.print("$" + i + " ");
+    }
+
+    for (int i = 0; i <= 1000; i++) {
+
+      System.out.print("#" + i + " ");
+    }
+
+    long endtime = System.currentTimeMillis();
+
+    System.out.println("total time taken  " + (endtime - startime));
+  }
+
+}
